bd37f32d9d9da5cef3416b2dbdc9657159ac1f89
Fabio Squizzato <fsquizzato@visualmedica.com>
2025-10-28 16:36:36 -0300

    new data

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -146,7 +146,7 @@ const MapComponent = () => {
         />
         
         {/* Renderizar rutas dinámicamente usando los primeros 4 elementos de combinedData */}
-        {combinedData.slice(0, 104).map((route, index) => (
+        {combinedData.slice(0, 117).map((route, index) => (
           <RoutingMachine 
             key={`${route.ruta}-${route.tramo}-${index}`}
             routeId={`route-${route.ruta}-${route.tramo}-${index}`}
